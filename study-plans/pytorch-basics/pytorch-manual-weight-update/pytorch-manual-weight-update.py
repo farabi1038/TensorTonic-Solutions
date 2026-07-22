4ae22a7e7745a37f41e9c1b40688d1f22ae5df4a
Ibne Farabi Shihab <ibnfarabishihab@gmail.com>
2026-07-22 13:50:41 -0700

    Solve pytorch-manual-weight-update

---
+++ study-plans/pytorch-basics/pytorch-manual-weight-update/pytorch-manual-weight-update.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+def manual_train_step(model, X, y, criterion, lr):
+    """
+    Returns: loss value as a Python float
+    """
+    output =model(X)
+
+    loss = criterion(output,y)
+    loss.backward()
+
+    with torch.no_grad():
+        for param in model.parameters():
+            param -=lr*param.grad
+            
+    model.zero_grad()
+
+    return loss.item()
+    
