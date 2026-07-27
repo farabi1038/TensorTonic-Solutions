3685e9c5b069a995bbed8b897d8eda9f73ed070e
Ibne Farabi Shihab <ibnfarabishihab@gmail.com>
2026-07-27 12:27:12 -0700

    Solve la-euclidean-distance

---
+++ study-plans/math-linear-algebra/la-euclidean-distance/la-euclidean-distance.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def euclidean_distance(x, y):
+    """
+    Returns: float, the Euclidean distance between x and y.
+    """
+    x=np.asarray(x,dtype=float)
+    y=np.asarray(y,dtype=float)
+
+    return float(np.linalg.norm(x-y))
